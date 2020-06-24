80d47f32d4d7b70be25a66d0a2a52d24238e6ee8
Alex Sadikov <sav1982@inbox.ru>
2020-06-24 16:44:08 +0300

    culc V.2

--- Calculator/src/App.js
+++ Calculator/src/App.js
@@ -6,27 +6,37 @@ class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      counter: 0,
-      num1: 0,
-      num2: 0
+      counter: '',
+      num1: '',
+      num2: ''
     }
   }
 
+  firstNum = (e) => {
+    this.setState({
+      num1: e.target.value
+    });
+  }
+
+  secondNum = (e) => {
+    this.setState({
+      num2: e.target.value
+    });
+  }
 
   click = () => {
     this.setState({
-    num1: Number(document.getElementById('num1').value),
-    num2: Number(document.getElementById('num2').value),
-    counter: this.state.num1 + this.state.num2
+    counter: Number(this.state.num1) + Number(this.state.num2)
     });
   }
+
   render () {
     return (
     <div>
-      <input type={Text} id="num1"/>
+      <input value={this.state.num1} onChange={this.firstNum} type="number" />
       <button onClick={this.click}>+</button>
-      <input type={Text} id="num2"/>
-    <div>={this.state.counter}</div>
+      <input value={this.state.num2} onChange={this.secondNum} type="number"/>
+      <div>={this.state.counter}</div>
     </div>
     );
   }
